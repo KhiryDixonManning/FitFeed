762b0f87566965d57f96f00923249cd07db93240
maxnchief <logandogan@gmail.com>
2026-04-01 16:15:38 -0500

    Remove unused useState hook from Feed component

--- fit-feed/src/pages/Feed.tsx
+++ fit-feed/src/pages/Feed.tsx
@@ -1,9 +1,7 @@
 
-import { useState } from "react";
 import Headbar from "../components/Headbar";
 
 export default function Feed() {
-  const [count, setCount] = useState(0);
   return (
     <div>
       <Headbar />
