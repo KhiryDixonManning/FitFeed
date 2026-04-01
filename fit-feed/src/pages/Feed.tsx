ff12d6e89708661bb1388810729eaa41758604f6
maxnchief <logandogan@gmail.com>
2026-04-01 16:14:57 -0500

    headbar added to feed page

--- fit-feed/src/pages/Feed.tsx
+++ fit-feed/src/pages/Feed.tsx
@@ -1,14 +1,12 @@
 
 import { useState } from "react";
+import Headbar from "../components/Headbar";
 
 export default function Feed() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Feed Page</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Test Button (Clicked {count} times)
-      </button>
+      <Headbar />
     </div>
   );
 }
