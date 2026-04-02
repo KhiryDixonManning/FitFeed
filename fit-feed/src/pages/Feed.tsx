1fd4db76818120043302c2caf6fd3405292b40ff
maxnchief <logandogan@gmail.com>
2026-04-02 13:42:40 -0500

    completed post

--- fit-feed/src/pages/Feed.tsx
+++ fit-feed/src/pages/Feed.tsx
@@ -1,11 +1,17 @@
 
 import Headbar from "../components/Headbar";
+import Post from "../components/Post";
 
 export default function Feed() {
   return (
     <div>
-      <Headbar />
-      <p>Feed Page</p>
+      <div>
+        <Headbar />
+        <p>Feed Page</p>
+      </div>
+      <div>
+        <Post />
+      </div>
     </div>
   );
 }
