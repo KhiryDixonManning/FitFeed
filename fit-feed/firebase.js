03bca59f3502dda397d81a4deb8673e16da9898d
Tsimmuaj Yang <tyang4204@gmail.com>
2026-04-02 15:13:20 +0000

    secured firebaseConfig

--- fit-feed/firebase.js
+++ fit-feed/firebase.js
@@ -8,13 +8,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: "fitfeed-67ee8",
-  storageBucket: "fitfeed-67ee8.firebasestorage.app",
-  messagingSenderId: "388462759865",
-  appId: "1:388462759865:web:0b860b66cba67cbf1e7068",
-  measurementId: "G-H70X69NC0J"
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.FIREBASE_SECRET,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
