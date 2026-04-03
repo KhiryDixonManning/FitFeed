0421d53bd4858b0ba9e0725e7ba6ad0b1a25eb83
maxnchief <logandogan@gmail.com>
2026-04-03 18:18:20 -0500

    added user profile page to differentiate and updated routing

---
+++ fit-feed/src/pages/UserProfile.tsx
@@ -0,0 +1,10 @@
+import Headbar from "../components/Headbar";
+
+export default function UserProfile() {
+  return (
+    <div>
+      <Headbar />
+      <p>User Profile Page</p>
+    </div>
+  );
+}
